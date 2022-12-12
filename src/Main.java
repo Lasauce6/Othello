2fa48f6e23edeafdfed354544e967cfeb05b18f4
Lasauce6 <lasaucisse006@gmail.com>
2022-12-12 13:38:05 +0100

    TODO

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,6 @@ public class Main {
     public static void main(String[] args) {
         Board board = new Board();
         Actions actions = new Actions(board);
+        actions.printBoard();
     }
 }
