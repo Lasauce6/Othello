f77056f9424a684c65c17c7814b601050a6a9203
Lasauce6 <lasaucisse006@gmail.com>
2022-12-18 16:56:44 +0100

    Colors & Clear console

--- src/othello/Actions.java
+++ src/othello/Actions.java
@@ -8,21 +8,38 @@ public class Actions {
     }
 
     public void printBoard() {
-        System.out.println("   A  B  C  D  E  F  G  H");
-        for (int i = 0; i < 8; i++) {
-            System.out.print(i + 1 + "  ");
-            for (int j = 0; j < board.getSize(); j++) {
-                if (board.getBoard()[i][j] == Color.BLACK) {
-                    System.out.print("B");
-                } else if (board.getBoard()[i][j] == Color.WHITE) {
-                    System.out.print("W");
+        String ANSI_RESET = "\u001B[0m";
+        String ANSI_BLACK = "\u001B[30m";
+        String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
+        System.out.println(ANSI_WHITE_BACKGROUND + ANSI_BLACK + "    A  B  C  D  E  F  G  H    " + ANSI_RESET);
+        int count = 1;
+        for (Color[] row : board.getBoard()) {
+            System.out.print(ANSI_WHITE_BACKGROUND + ANSI_BLACK + " " + count + "  ");
+            for (Color color : row) {
+                if (color == Color.BLACK) {
+                    System.out.print(ANSI_BLACK + "●");
+                } else if (color == Color.WHITE) {
+                    String ANSI_WHITE = "\u001B[97m";
+                    System.out.print(ANSI_WHITE + "●");
                 } else {
-                    System.out.print("X");
+                    System.out.print(" ");
                 }
                 System.out.print("  ");
             }
-            System.out.println(i + 1);
+            System.out.println(ANSI_WHITE_BACKGROUND + ANSI_BLACK + count + " " + ANSI_RESET);
+            count++;
         }
-        System.out.println("   A  B  C  D  E  F  G  H");
+        System.out.println(ANSI_WHITE_BACKGROUND + ANSI_BLACK + "    A  B  C  D  E  F  G  H    " + ANSI_RESET);
+    }
+
+    public void clearScreen() { // ne fonctionne pas sur un IDE
+        try {
+            final String os = System.getProperty("os.name");
+            if (os.contains("Windows")) Runtime.getRuntime().exec("cls");
+            else Runtime.getRuntime().exec("clear");
+        } catch (final Exception e) {
+            System.out.println("Error: " + e);
+        }
     }
 }
