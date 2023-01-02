2c5aea95182c1f95549fab7021562513f4f75f36
Lasauce6 <lasaucisse006@gmail.com>
2023-01-02 16:53:40 +0100

    Javadoc

--- README.md
+++ README.md
@@ -14,15 +14,13 @@ We are 2 students from UPEC (Paris-Est Créteil University)
 We made this project for our third semester.
 
 ## Todo list : 
-- [x] Implémenter le plateau de jeu
+- [X] Implémenter le plateau de jeu
 - [X] Implémenter les algorithmes de logique de jeu
 - [ ] Implémenter l'algorithme de l'IA
-- [x] Implémenter l'interface graphique
+- [ ] Implémenter l'interface graphique
 
 
-consigne :
-un fichier README.txt donnant un compte rendu honnête et fiable, en 10
-à 20 lignes, de votre niveau d’avancement par rapport au travail demandé
-et des principales difficultés rencontrées 
+## Compte rendu :
+Le jeu est fonctionel mais l'IA n'est pas encore implémentée.
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,16 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Scanner;
-
 import othello.Actions;
 import othello.Board;
 import othello.Color;
 
 public class Main {
+
     /**
      * Lancement du menu principal
-     * 
-     * read()  = lire un caractères
-     * readline()  = lire une chaine de caractères
-     * skip(n) = ignore n car
      */
-
-    public static void main(String[] args) throws Exception
-    {InputStreamReader isr = new InputStreamReader(System.in);
-        BufferedReader br = new BufferedReader(isr); //attention synchrone
-        //Scanner sc = new Scanner(); //attention asynchrone donc + rapide car n'attend pas la finalisation des taches
-        
-
-        Board board = new Board(4);
+    public static void main(String[] args) {
+        Board board = new Board(8);
         Actions actions = new Actions(board);
         actions.play(Color.BLACK);
     }
+
 }
