7bbff8d1525394791075e44b9b4273bc4d3338e6
juSnior <ugo.colosetti@etu.u-pec.fr>
2023-01-03 13:38:44 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,3 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Scanner;
-
 import othello.Actions;
 import othello.Board;
 import othello.Color;
@@ -9,18 +5,9 @@ import othello.Color;
 public class Main {
     /**
      * Lancement du menu principal
-     * 
-     * read()  = lire un caractères
-     * readline()  = lire une chaine de caractères
-     * skip(n) = ignore n car
      */
 
-    public static void main(String[] args) throws Exception
-    {InputStreamReader isr = new InputStreamReader(System.in);
-        BufferedReader br = new BufferedReader(isr); //attention synchrone
-        //Scanner sc = new Scanner(); //attention asynchrone donc + rapide car n'attend pas la finalisation des taches
-        
-
+    public static void main(String[] args) {
         Board board = new Board(4);
         Actions actions = new Actions(board);
         actions.play(Color.BLACK);
