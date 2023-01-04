e0d4756acc0284ca1e9c5ccf5ceb506bdd990cae
Raphaël BATICLE <61377616+Lasauce6@users.noreply.github.com>
2023-01-04 10:05:33 +0100

    Revert Javadoc + consigne.pdf

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,6 @@ public class Main {
     /**
      * Lancement du menu principal
      */
-
-/**
- * 
- *     public static void main(String[] args) {
-        Board board = new Board(4);
-    } 
-
- */
-
     public static void main(String[] args) {
         Board board = new Board(8);
         Actions actions = new Actions(board);
